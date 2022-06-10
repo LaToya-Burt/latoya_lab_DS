7a8a90fca6ac438c51248ce5d3157eb37649598a
JJ <jj.vega86@gmail.com>
2022-06-10 14:54:17 -0400

    Added source files to repository

---
+++ exercises.py
@@ -0,0 +1,91 @@
+# Problem 1 (Sets)
+
+"""
+Create a new Set with string values that represent colors
+Loop through the Set and print each value
+"""
+
+
+def print_colors(set_of_colors):
+    # This code is not complete!
+    # Make sure to loop through this set as directed
+    # And print each color one at a time
+    print(set_of_colors)
+
+
+# Problem 2 (Sets)
+
+"""
+Take the Set from Problem 1
+Take input from the user, asking for their favorite color
+Check to see if their favorite color is in the Set
+If the color is in the Set, print "We found your favorite color!"
+If the color is not in the Set, add that color to the Set
+"""
+
+
+def find_favorite_color(set_of_colors):
+    pass
+
+
+# Problem 3 (Sets)
+"""
+Take the Set from Problem 1
+Display the contents to the user in the terminal
+Ask the user what colors they would like to add
+Create a NEW Set with colors the user adds
+Then utilize the `union` Set method to combine sets
+display the new Set
+See: https://www.w3schools.com/python/python_sets_join.asp
+"""
+
+
+def add_colors(set_of_colors):
+    pass
+
+
+# Recommend having this resource open for Problems 4-6
+# https://www.w3schools.com/python/python_tuples.asp
+
+# Problem 4 (Tuples)
+
+"""
+Write a Tuple that contains strings that represent coins
+There will be three types of coins: "gold", "silver", and "copper"
+For example: ("gold", "silver", "copper", "gold")
+Note that there are duplicates (imagine coins in a purse)
+Next, write a function that displays all coins using a WHILE loop
+
+"""
+
+
+def display_coins(coins):
+    pass
+
+
+# Problem 5 (Tuples)
+
+"""
+Take the Tuple from Problem 4 and add a new coin to it
+Then, display all of the coins in the Tuple after adding the coin
+NOTE: You will need to convert your Tuple to a List, then back to a Tuple in order to do this!
+See: https://www.w3schools.com/python/python_tuples_update.asp
+"""
+
+
+def add_coin(coins):
+    pass
+
+
+# Problem 6 (Tuples)
+
+"""
+Take the Tuple from Problem 4 and remove all of the duplicates from it
+Then, display all of the "coin types" to the user
+Example Output: "gold, silver, copper"
+NOTE: You will need to convert your Tuple to a Set, then back toa Tuple in order to de-duplicate
+"""
+
+
+def display_coin_types(coins):
+    pass
---
+++ main.py
@@ -0,0 +1,14 @@
+import exercises
+
+# Problems 1-3
+colors = {"blue", "red", "green", "tomato"}
+exercises.print_colors(colors)
+# print(exercises.find_favorite_color(colors))
+# exercises.add_colors(colors)
+
+coins = ("gold", "silver", "copper", "gold")
+# Problems 4-6
+# exercises.display_coins(coins)
+# exercises.add_coin(coins)
+# exercises.display_coin_types(coins)
+
